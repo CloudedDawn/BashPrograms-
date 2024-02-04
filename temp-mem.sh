a47f993d59f1dd05fe117124aa41ce1ffae91713
CloudedDawn <138307485+CloudedDawn@users.noreply.github.com>
2024-02-04 09:31:45 +0000

    Update temp-mem.sh fix for all users

--- temp-mem.sh
+++ temp-mem.sh
@@ -2,16 +2,18 @@
 echo "This program makes a temporary ram disk of 3gb"
 echo "The disk is stored in your ram"
 echo "To prevent data loss back up your data"
-sudo umount /home/${USER}/.cache
-sudo umount /home/${USER}/Desktop/temp
-rm -rf /home/${USER}/.cache
-sudo mkdir /home/${USER}/.cache
-sudo mount -t tmpfs -o size=1g tmpfs /home/${USER}/.cache
-rm -rf /home/${USER}/Desktop/temp
-mkdir /home/${USER}/Desktop/temp
-sudo mount -t tmpfs -o size=3g tmpfs /home/${USER}/Desktop/temp
-sudo chown -R ${USER} /home/${USER}/Desktop/temp
-sudo chown -R ${USER} /home/${USER}/.cache
-chmod -R 700 /home/${USER}/Desktop/temp
-chmod -R 700 /home/${USER}/.cache
+echo "Enter the current user"
+read a
+sudo umount /home/${a}/.cache
+sudo umount /home/${a}/Desktop/temp
+rm -rf /home/${a}/.cache
+sudo mkdir /home/${a}/.cache
+sudo mount -t tmpfs -o size=1g tmpfs /home/${a}/.cache
+rm -rf /home/${a}/Desktop/temp
+mkdir /home/${a}/Desktop/temp
+sudo mount -t tmpfs -o size=3g tmpfs /home/${a}/Desktop/temp
+sudo chown -R ${a} /home/${a}/Desktop/temp
+sudo chown -R ${a} /home/${a}/.cache
+chmod -R 700 /home/${a}/Desktop/temp
+chmod -R 700 /home/${a}/.cache
 exit 0
